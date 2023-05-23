7ed054a44622e04d9861ac1c843a1c1bf9163d49
Ltooelst <116316691+Ltooelst@users.noreply.github.com>
2023-05-22 20:35:48 -0500

    Add files via upload

---
+++ cloak.js
@@ -0,0 +1,36 @@
+let inFrame
+
+try {
+    inFrame = window !== top
+} catch (e) {
+    inFrame = true
+}
+
+if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+    const popup = open("about:blank", "_blank")
+    if (!popup || popup.closed) {
+        alert("Allow popups and redirects to hide this from showing up in your history.")
+    } else {
+        const doc = popup.document
+        const iframe = doc.createElement("iframe")
+        const style = iframe.style
+        const link = doc.createElement("link")
+
+        const name = localStorage.getItem("name") || "‌";
+        const icon = localStorage.getItem("icon") || "https://harmonious-brigadeiros-09b6fe.netlify.app/icons/a.ico";
+        
+        doc.title = name;
+        link.rel = "icon";
+        link.href = icon;
+        
+        iframe.src = location.href 
+        style.position = "fixed"
+        style.top = style.bottom = style.left = style.right = 0
+        style.border = style.outline = "none"
+        style.width = style.height = "100%"
+
+        doc.head.appendChild(link);
+        doc.body.appendChild(iframe)
+        location.replace("https://google.com")
+    }
+}
